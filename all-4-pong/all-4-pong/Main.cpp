90fa7ecf6abcfc0be772f393744b7a26101df1cc
Jposc539 <jposc@gmail.com>
2013-10-30 21:48:23 -0400

    Added unit test to new a4p version

--- all-4-pong/all-4-pong/Main.cpp
+++ all-4-pong/all-4-pong/Main.cpp
@@ -7,18 +7,12 @@
 #include "start.h"
 #include "ingame.h"
 
-class World;
-void unitTest(Start* w) {
-    //assert(World::getInstance());
-    w->StartingMethod();
-    assert(World::getInstance().getGamePlayers().size() == 4);
-    assert(World::getInstance().getDifficulty() == 1);
+void unitTest() {
+
+    assert(World::getInstance().getDifficulty() == 0);
 
 
     cout << "All unit tests pass!" << endl;
-
-    w->close();
-
 }
 
 
@@ -30,10 +24,13 @@ int main(int argc, char *argv[])
     Start Pong;
     Pong.show();
 
-//    Start w;
-  //  if (main) {
-    //    unitTest(&w);
-   // }
+    vector<string> args(argv, argv+argc); // A vector of command line arguments
+
+        if (args.size() == 2) { // Checks it the user entered a second command line argument
+            if (args.at(1) == "-test") { // Checks if the user entered the -test flag
+                unitTest(); // Run unit tests
+            }
+        }
 
 
     return a.exec();
