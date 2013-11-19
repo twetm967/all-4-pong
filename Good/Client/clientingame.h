106bc6885933952cf25246aea20e17eefaa64495
User <User@bju.edu>
2013-11-19 18:27:02 -0500

    Hello

--- Good/Client/clientingame.h
+++ Good/Client/clientingame.h
@@ -14,7 +14,7 @@ class clientingame;
 class clientingame : public QWidget
 {
     Q_OBJECT
-    //test
+    //test here
 public:
     explicit clientingame(QWidget *parent = 0);
     ~clientingame();
